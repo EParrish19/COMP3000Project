ba0b15043a3bb6ec6d33829368e5c377bf7e4817
EParrish19 <ethan.parrish@students.plymouth.ac.uk>
2021-11-18 22:25:36 +0000

    Started script to control shooting, created simple test scene

---
+++ Assets/Scripts/shootProjectile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity;
+using UnityEditor;
+
+public class shootProjectile : MonoBehaviour
+{
+    //Is a target in sight of the entity
+    bool targetSighted = false;
+
+    private GameObject thisEntity;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
