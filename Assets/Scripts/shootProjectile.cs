85c714a77350aa718be42685c535f8585f6c4a0d
EParrish19 <ethan.parrish@students.plymouth.ac.uk>
2021-11-19 15:44:39 +0000

    Created concept/test scenes for different enemy elevations, started shootProjectile script

--- Assets/Scripts/shootProjectile.cs
+++ Assets/Scripts/shootProjectile.cs
@@ -10,10 +10,71 @@ public class shootProjectile : MonoBehaviour
     bool targetSighted = false;
 
     private GameObject thisEntity;
+    private GameObject target;
+
+    //on start, stores the current gameobject as a variable
+    private void Start()
+    {
+        thisEntity = gameObject;
+    }
+
+    //setter method to set the current target for the entity
+    public void setTarget(GameObject newTarget)
+    {
+        target = newTarget;
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (target != null)
+        {
+
+            //RaycastHit used to get the target name when 
+            RaycastHit targetInSight;
+
+
+
+            //ray used as an indicator of if the target can be seen
+            Ray sight = new Ray(thisEntity.transform.position, target.transform.position);
+
+            //does a raycast to stored target
+            if (Physics.Raycast(sight, out targetInSight, 100000))
+            {
+
+                //if the raycast hits the right target, print to console the event
+                if (targetInSight.transform.parent.name != null && targetInSight.transform.parent.name == target.transform.parent.name)
+                {
+                    Debug.Log(targetInSight.transform.parent.name + " sighted by: " + thisEntity.name);
+                    targetSighted = true;
+                }
+                else if (targetInSight.transform.parent.name != null)
+                {
+                    Debug.Log("Target Obstructed");
+                }
+                else
+                {
+                    Debug.Log("Target not Found");
+                }
+            }
+
+            //seperate ray for the actual projectile
+            Ray projectile = new Ray();
+
+            //only needs to run if the target is in sight
+            if (targetSighted == true)
+            {
+                //vector3 used to add randomness to each shot
+                Vector3 randomAngle = Random.rotation.eulerAngles;
+
+                projectile.direction = sight.direction;
+                projectile.direction.Scale(randomAngle);
+
+
+            }
+        }
     }
+
+
 }
