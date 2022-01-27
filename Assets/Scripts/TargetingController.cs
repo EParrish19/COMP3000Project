1ff2f23f0f090f60fd7dc521b6ea992911affae2
(s) Ethan Parrish <eparrish@SOCNET.SOC.PLYMOUTH.AC.UK>
2022-01-27 11:32:44 +0000

    Added automatic target change to friendly units based on distance and visibility to targeting controller

--- Assets/Scripts/TargetingController.cs
+++ Assets/Scripts/TargetingController.cs
@@ -5,39 +5,90 @@ using UnityEngine;
 public class TargetingController : MonoBehaviour
 {
 
-    private List<GameObject> enemyUnits = new List<GameObject>();
-    private List<GameObject> friendlyUnits;
-    private List<Vector3> friendlyPositions;
-    private int[] enemyDistances;
+    private List<GameObject> friendlyUnits = new List<GameObject>();
+
+    private sight sightScript;
+
+    [SerializeField]
+    float updateTargetTime = 2.0f;
 
     // Start is called before the first frame update
-    void Start()
+    void FixedUpdate()
     {
-
+        if (!IsInvoking("UpdateTargets"))
+        {
+            Invoke("UpdateTargets", updateTargetTime);
+        }
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void UpdateTargets()
     {
-        foreach(GameObject friendly in GameObject.FindGameObjectsWithTag("FriendlyUnits"))
+        foreach (GameObject friendly in GameObject.FindGameObjectsWithTag("FriendlyUnit"))
         {
             friendlyUnits.Add(friendly);
+            Debug.Log("friendly unit added: " + friendly.name);
         }
 
-        foreach(GameObject unit in friendlyUnits)
+        foreach(GameObject friendly in friendlyUnits)
         {
-            friendlyPositions.Add(unit.transform.position);
-        }
+            List<GameObject> enemyUnits = new List<GameObject>();
 
-        foreach (Vector3 friendlyPosition in friendlyPositions)
-        {
-            foreach (Collider entity in Physics.OverlapSphere(friendlyPosition, 50f))
-            {
-                if (entity.gameObject.tag == "EnemyUnits")
+            sightScript = friendly.GetComponent<sight>();
+
+            Debug.Log(friendly.name + " is searching for targets");
+            foreach (Collider enemy in Physics.OverlapSphere(friendly.transform.position, 50f))
+            { 
+
+                if (enemy.gameObject.tag == "EnemyUnit")
                 {
-                    enemyUnits.Add(entity.gameObject);
+                    enemyUnits.Add(enemy.gameObject);
+                    Debug.Log("enemy unit added: " + enemy.gameObject.name);
                 }
             }
+
+            foreach(GameObject enemy in enemyUnits)
+            {
+                RaycastHit visible;
+                Vector3 direction = (enemy.transform.position - friendly.transform.position).normalized;
+
+                if(Physics.Raycast(friendly.transform.position, direction, out visible))
+                {
+                    if(visible.transform.gameObject != enemy)
+                    {
+                        enemyUnits.Remove(enemy);
+                        Debug.Log("enemy unit removed: " + enemy.name);
+                    }
+                }
+            }
+
+            GameObject closestEnemy = null;
+            float closestEnemyDistance = 0.0f;
+
+            foreach(GameObject enemy in enemyUnits)
+            {
+                if(closestEnemy == null)
+                {
+                    closestEnemy = enemy;
+                    closestEnemyDistance = Vector3.Distance(friendly.transform.position, enemy.transform.position);
+                }
+                else
+                {
+                   float newDistance = Vector3.Distance(friendly.transform.position, enemy.transform.position);
+
+                    if (closestEnemyDistance > newDistance)
+                    {
+                        closestEnemy = enemy;
+                        closestEnemyDistance = newDistance;
+                    }
+                }
+
+                Debug.Log("Closest enemy to " + friendly.name + " is " + closestEnemy.name);
+            }
+
+            sightScript.SetTarget(closestEnemy);
+
         }
+
     }
 }
