8859d1a8a38b0016038a8a4d2b827a0b98f62f81
EParrish19 <ethan.parrish@students.plymouth.ac.uk>
2022-03-30 22:26:13 +0100

    Camera Movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Transform playerCamera;
+
+    [SerializeField]
+    private GameObject player;
+
+    private Vector3 newCameraPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = Camera.main.transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newCameraPos = player.transform.position;
+        newCameraPos.y += 23.75f;
+
+        playerCamera.position = newCameraPos;
+    }
+}
